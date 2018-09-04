ff517a3ac4087643550e2105ed6ff74ed23b3bcb
Sergey <s.kravtsov@hotmail.com>
2018-09-04 12:14:24 +0300

    оживление интерактивных элементов на JS. Правки по html/css

--- js/script.js
+++ js/script.js
@@ -1,35 +1,107 @@
-var link1 = document.querySelector(".map");
-var link2 = document.querySelector(".write-us");
+var linkMap = document.querySelector(".map");
+var linkWriteUs = document.querySelector(".write-us");
 
-var popup1 = document.querySelector(".modal-map");
-var popup2 = document.querySelector(".modal-write-us");
+var popupMap = document.querySelector(".modal-map");
+var popupWriteUs = document.querySelector(".modal-write-us");
 
-var close1 = document.querySelector(".btn-close-1");
-var close2 = document.querySelector(".btn-close-2");
+var closeMap = popupMap.querySelector(".btn-close");
+var closeWriteUs = popupWriteUs.querySelector(".btn-close");
 
-link1.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup1.classList.add("modal-show");
-})
+var form = popupWriteUs.querySelector("form");
+var userName = popupWriteUs.querySelector(".name");
+var email = popupWriteUs.querySelector("[name=email]");
+var letter = popupWriteUs.querySelector("[name=text-mail]");
 
-link2.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup2.classList.add("modal-show");
-    if (Storage) {
-      name.value = Storage;
-      email.focus();
-    } else if (Storage) {
-      email.value = Storage;
-      text-mail.focus();
+var isStorageSupport = true;
+var storage = "";
+
+var overlay = document.querySelector(".modal-overlay");
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+//MODAL-MAP
+
+linkMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.add("modal-show");
+  overlay.classList.add("modal-overlay-show");
+});
+
+closeMap.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("modal-show");
+  overlay.classList.remove("modal-overlay-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupMap.classList.contains("modal-show")) {
+      popupMap.classList.remove("modal-show");
+      overlay.classList.remove("modal-overlay-show");
     }
+  }
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupMap.classList.remove("modal-show");
+  overlay.classList.remove("modal-overlay-show");
 })
 
-close1.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup1.classList.remove("modal-show");
-})
+//MODAL WRITE US
 
-close2.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup2.classList.remove("modal-show");
+linkWriteUs.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWriteUs.classList.add("modal-show");
+  overlay.classList.add("modal-overlay-show");
+
+  if (storage) {
+    userName.value = storage;
+    email.focus();
+  } else {
+    userName.focus();
+  }
+});
+
+closeWriteUs.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWriteUs.classList.remove("modal-show");
+  popupWriteUs.classList.remove("modal-error");
+  overlay.classList.remove("modal-overlay-show");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!userName.value || !email.value || !letter.value) {
+    evt.preventDefault();
+    popupWriteUs.classList.remove("modal-error");
+    popupWriteUs.offsetWidth = popupWriteUs.offsetWidth;
+    popupWriteUs.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("userName", userName.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupWriteUs.classList.contains("modal-show")) {
+      popupWriteUs.classList.remove("modal-show");
+      popupWriteUs.classList.remove("modal-error");
+      overlay.classList.remove("modal-overlay-show");
+    }
+  }
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupWriteUs.classList.remove("modal-show");
+  popupWriteUs.classList.remove("modal-error");
+  overlay.classList.remove("modal-overlay-show");
 })
